50ba95a53a2df8f362b63b1c0883d997b5ceb4fa
James Womack <womackrjames@gmail.com>
2017-11-25 17:45:45 -0800

    Initial commit of blender addon

---
+++ __init__.py
@@ -0,0 +1,20 @@
+bl_info = {
+    "name": "Turn Engine Exporter Tools",
+    "author": "Scroopid",
+    "version": (1, 0, 0),
+    "blender": (2, 79, 0),
+    "location": "File > Export > Turn Engine (.model)",
+    "description": "Tools for exporting meshes into Turn Engine .model archives with engine-optimized format.",
+    "warning": "WIP, when the export tools are finished and integrated into the engine, this will be removed",
+    "category": "Import-Export"
+}
+
+import bpy
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
